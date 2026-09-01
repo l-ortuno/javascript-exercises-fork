aa454bcc5c85b26dcebcdaa24814ecc622b9781a
l-ortuno <297383124+l-ortuno@users.noreply.github.com>
2026-09-01 19:12:11 +0200

    Follow instructions on foundations/06_repeatString.
    Read the readme, create and test the string.

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,23 @@
-const repeatString = function() {
+//1. coge el string
+//2. lo repite el numero de veces que le pongas
+//3. devuelve el resultado
 
+
+const repeatString = function(str, num) {
+	{
+		 if (num>0){
+			let result = "";
+			for(let i = 0; i < num; i ++)
+			result=result+str
+			return result
+			}
+		else if (num<0){
+			return "ERROR"
+	  	  }
+		else if (num===0){
+			return ""
+		}
+	} 
 };
 
 // Do not edit below this line
