c922d7da2bae4d99fccfacd468e8864f9f076fc1
l-ortuno <297383124+l-ortuno@users.noreply.github.com>
2026-09-01 19:45:22 +0200

    Follow instructions onfoundations/07_reverseString.
    Read the readme, create function ant test it a million times till it worked.

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let result= ''
+    for (let i=string.length -1; i>=0; i--){
+        result=result+string.charAt(i)
+    }
 
-};
-
+    return result
+}
 // Do not edit below this line
 module.exports = reverseString;
