2c8c15f9089b827a8ec67dd5dbaad4d12806a657
l-ortuno <297383124+l-ortuno@users.noreply.github.com>
2026-09-03 19:14:58 +0200

    Achieve foundations/08_removeFromArray
    Read the README, followed instructions and wrote the function.

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
 const removeFromArray = function() {
+   let args = Array.from(arguments);
+   let popped = args.slice(1);
+
+   for (let i = 0; i < args[0].length; i++) {
+      let current = args[0][i];
+
+      if (popped.includes(current)) {
+         args[0].splice(i, 1);
+         i--
+      }
+    
+   }
+    return args[0];
 };
 
 // Do not edit below this line
