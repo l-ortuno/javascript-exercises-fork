96779e9bc2ed39ed64f7c3bbf64b9957b58ce55b
l-ortuno <297383124+l-ortuno@users.noreply.github.com>
2026-09-03 19:43:23 +0200

    Do exercise foundations/09_sumAll
    Read the README, googled to learn a new function and wrote it.

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
 
+if ((!Number.isInteger (start)) || (!Number.isInteger (end)) || (!Number.isFinite (start)) || (!Number.isFinite (end))) {
+    return 'ERROR'
+}
+
+else {
+
+    const min = Math.min(start, end);
+    const max = Math.max(start, end);
+  
+  if ((min >= 0) && (max >= 0)) {
+    return ((max - min + 1) * (min + max)) / 2;
+  }
+  else if (min < 0) {
+    return 'ERROR'
+  }
+}
 };
 
 // Do not edit below this line
